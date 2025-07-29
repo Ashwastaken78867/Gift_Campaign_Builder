0e80d2f4628b680c3aa83fbf8637b7c33f2173cd
Ash Bagda <abagda34@gmail.com>
2025-07-29 18:34:57 +0530

    Initial commit with Vite setup

---
+++ src/App.jsx
@@ -0,0 +1,10 @@
+function App() {
+  return (
+    <div className="p-6 bg-gray-100 min-h-screen">
+      <h1 className="text-2xl font-bold mb-4">🎁 Gifting Campaign Builder</h1>
+      {/* We’ll add StepList component here later */}
+    </div>
+  );
+}
+
+export default App;
